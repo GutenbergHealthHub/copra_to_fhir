ed7678a3f66cfe0d3b9f0a138201e542ee2e39c6
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-02-03 13:53:47 +0100

    database and data analysis updated.

--- data_analysis.sql
+++ data_analysis.sql
@@ -13,11 +13,24 @@ join icu_copra.copra_config_vars v
   on v.description = c.copra_names_descriptions or v."name" = c.copra_names_descriptions
 where v.parent in (1, 20)
 and v.co6_config_variabletypes_id in (3, 6, 12)
+and not c.deleted
 order by c.score_set desc, 
 p.profiles
 ;
 
-select * from icu_copra.v_python_match vpm where vpm.score_set = 100;
+--truncate icu_copra.python_match;
+
+-- Procedure
+
+-- update icu_copra.python_match set deleted = true where profile_names_loinc_short_names = 'Extrakorporale Verfahren';
+-- update icu_copra.python_match set deleted = true where profile_names_loinc_short_names = 'Beatmung';
+
+-- DeviceMetric
+-- update icu_copra.python_match set deleted = true where profile_names_loinc_short_names = 'Eingestellte und gemessene Parameter';
+
+
+
+select * from icu_copra.v_python_match vpm where vpm.score_set > 89;
 
 select * from icu_copra.copra_config_vars ccv order by id_syst;
 
