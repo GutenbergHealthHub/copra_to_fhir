a70316ed96fddb75e1701af5706b6a3eed2486be
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-02-06 10:13:52 +0100

    some match seted to true

--- data_analysis.sql
+++ data_analysis.sql
@@ -30,12 +30,66 @@ p.profiles
 -- update icu_copra.python_match set deleted = true where profile_names_loinc_short_names = 'Eingestellte und gemessene Parameter';
 
 
-update icu_copra.python_match set deleted = false where profile_names_loinc_short_names = 'Arterieller Druck' and score_set = 100;
 select * from icu_copra.v_python_match vpm where vpm.score_set > 89;
 
-select * from icu_copra.copra_config_vars ccv order by id_syst;
 
-select * from icu_copra.python_match pm where copra_names_descriptions = 'PICCO_ABP'
+-- Arterieller Druck
+select * from icu_copra.python_match where profiles = 'Arterieller Druck';
 
-alter table icu_copra.python_match rename to python_match_tmp;
+update icu_copra.python_match set deleted = true where profiles = 'Arterieller Druck' and ("name" <> 'PICCO_ABP' or name not like '%artDruck');
+update icu_copra.python_match set deleted = true where name = 'PICCO_ABP' and score_set <> 100; 
 
+
+-- Atemfrequenz
+
+select * from icu_copra.python_match pm where profiles = 'Atemfrequenz' order by score_set desc;
+
+update icu_copra.python_match 
+set deleted = true where profiles = 'Atemfrequenz' and name not in ('Beatmung_MS_Pallas_Frequenz', 'Beatmung_MS_BiPAPV_AF', 
+  'Beatmung_MS_Evita2_frequenz', 'Beatmung_MS_VisionA_BreathRate', 'Beatmung_MS_Avea_Frequenz', 'P_Beatmung_MS_C3_fTotal', 
+  'Beatmung_MS_T1_fTotal', 'AF');
+
+-- Blutdruck
+
+select * from icu_copra.python_match pm where profiles = 'Blutdruck' order by score_set desc;
+
+update icu_copra.python_match set deleted = true where profiles = 'Blutdruck' and name not in ('P_NBP_reArm',
+  'P_NBP_reBein', 'NBP_1', 'NBP', 'P_NBP_liBein', 'P_NBP_liArm');
+ 
+
+-- Dynamische Kompliance
+
+select * from icu_copra.python_match pm where profiles = 'Dynamische Kompliance' order by score_set desc;
+
+update icu_copra.python_match set deleted = true where profiles = 'Dynamische Kompliance' and name not in (
+  'Beatmung_Messung_Compliance', 'Beatmung_MS_Evita2_Compliance', 'Beatmung_MS_Evita4_Compliance'
+);
+
+
+-- Herzfrequenz
+
+select * from icu_copra.python_match pm where profiles = 'Herzfrequenz' order by score_set desc;
+
+update icu_copra.python_match set deleted = true where profiles = 'Herzfrequenz' and name not in (
+  'HF', 'PICCO_HF'
+);
+
+
+
+-- Herzzeitvolumen
+
+select * from icu_copra.python_match pm where profiles = 'Herzzeitvolumen' order by score_set desc;
+
+update icu_copra.python_match set deleted = true where profiles = 'Herzzeitvolumen' and name not in (
+  'HZV', 'PICCO_HZV', 'VigilanceC_HZV'
+);
+
+-- Inspiratorische Sauerstofffraktion gemessen
+
+select * from icu_copra.python_match pm where profiles = 'Inspiratorische Sauerstofffraktion gemessen' order by score_set desc;
+
+update icu_copra.python_match set deleted = true where profiles = 'Inspiratorische Sauerstofffraktion gemessen' and name not in (
+  'Beatmung_Messung_FiO2', 'Beatmung_MS_Avea_FiO2', 'Beatmung_MS_Evita4_FiO2', 'Beatmung_MS_Pallas_O2_insp'
+);
+
+select * from icu_copra.python_match pm where score_set = 100 and not deleted order by profiles;
