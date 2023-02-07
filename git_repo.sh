92ca4f2446802bddf9dc75c7c8e23157800a2e9a
Abel Hodelin Hernandez <abel.hodelin.hernandez@gmail.com>
2023-02-07 14:27:57 +0100

    Last update Di 7. Feb 14:27:57 CET 2023

--- git_repo.sh
+++ git_repo.sh
@@ -1,4 +1,7 @@
-pg_dump > /home/ahodelin/git_repos/copra_to_fhir/copra_to_fhir.sql -U her11a_admin -d copra_to_fhir -O -x
+
+dbpass=`pass db/her11a_admin`
+export PGPASSWORD=$dbpass
+pg_dump > /home/ahodelin/git_repos/copra_to_fhir/copra_to_fhir.sql -U her11a_admin -d copra_to_fhir -O -x -w
 git add *
 d=`date`
 git commit -m "Last update $d"
